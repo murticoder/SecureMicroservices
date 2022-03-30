18c4e3d0080bbaff5482b36c522c53f8119deca4
murtaza-yazilim2 <yazilim2@birfatura.com>
2022-03-30 13:27:24 +0300

    Movies API Completed

--- Movies.API/Program.cs
+++ Movies.API/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Movies.API.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,9 @@ namespace Movies.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            SeedDatabase(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +26,12 @@ namespace Movies.API
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        private static void SeedDatabase(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var moviesContext = services.GetRequiredService<MoviesAPIContext>();
+            MoviesContextSeed.SeedAsync(moviesContext);
+        }
     }
 }
